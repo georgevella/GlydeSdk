12ba9372d9c7d6c1825654de3761199105185cf1
George Vella <george.vella@betssongroup.com>
2017-05-10 22:47:09 +0200

    Added configuration bootstrapping stage

---
+++ src/Glyde.Configuration/Bootstrapping/ConfigurationBootstrapperStage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Glyde.Bootstrapper;
+using Glyde.Configuration.Models;
+
+namespace Glyde.Configuration.Bootstrapping
+{
+    public class ConfigurationBootstrapperStage : IBootstrapperStage
+    {
+        /// <inheritdoc />
+        public void RunStageBootstrappers(IGlydeApplication app, IEnumerable<Assembly> assemblies)
+        {
+            var configurationSectionTypes = assemblies.SelectMany(a => a.DefinedTypes.Where(t => IsConfigurationSection(t.AsType()))).ToList();
+
+
+        }
+
+        private bool IsConfigurationSection(Type type)
+        {
+            var typeinfo = type.GetTypeInfo();
+            if (typeinfo.IsAbstract)
+                return false;
+
+            return InheritsFromConfigurationSection(type);
+        }
+
+        private bool InheritsFromConfigurationSection(Type type)
+        {
+            if (type == typeof(object))
+                return false;
+
+            if (type == typeof(ConfigurationSection))
+                return true;
+
+            var typeinfo = type.GetTypeInfo();
+
+            return InheritsFromConfigurationSection(typeinfo.BaseType);
+        }
+    }
+}
